f08899b0be1fc1cd265c814433de321e9051c0c6
limiteddata <limiteddata09@gmail.com>
2021-11-01 15:24:28 +0200

    test2

--- utils/build.js
+++ utils/build.js
@@ -3,6 +3,23 @@ process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 process.env.ASSET_PATH = '/';
 
+const { exec } = require("child_process");
+const { exit } = require("process");
+const arg2 = process.argv[2].split('=');
+if(arg2[0] === 'release'){
+  exec(`npm version ${arg2[1]}`, (error, stdout, stderr) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          exit();
+      }
+      if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          exit();
+      }
+      console.log(`stdout: ${stdout}`);
+  });
+}
+
 var webpack = require('webpack'),
   config = require('../webpack.config');
 
